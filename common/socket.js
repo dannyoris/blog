665045a3111db2a7b9c147561d32435f558d838c
danny <dannyb.liu@sh.leoburnett.com>
2013-11-09 22:09:37 +0800

    reg&loginRight

--- common/socket.js
+++ common/socket.js
@@ -1,10 +1,17 @@
 var socket = module.exports = exports = {};
 var socketio = require('socket.io');
+var chat = require('../models/chat.js');
 
 socket.start = function(server){
 	socketio.listen(server).on('connection', function (socket) {
 	    socket.on('message', function (msg) {
-	        socket.broadcast.emit('message', msg);
+	    	var newChat = new chat(msg);
+	    	newChat.save(function(res){
+	    		if(res==null){
+	    			socket.broadcast.emit('message', msg);
+	    		}
+	    	});
+	        
 	    });
 	});
 }
--- models/chat.js
+++ models/chat.js
@@ -1,17 +1,20 @@
 var mongodb = require('./db');
-
+var date = new Date();
 var time = {
   date: date,
   year : date.getFullYear(),
   month : date.getFullYear() + "-" + (date.getMonth()+1),
   day : date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate(),
-  minute : date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes()
+  minute : date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes(),
+  second : date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
 }
 
 function Chat(info){
 	this.username = info.username;
 	this.message = info.message;
 }
+
+module.exports = Chat;
 
 Chat.prototype.save = function(callback){
 	var info = {
@@ -24,13 +27,34 @@ Chat.prototype.save = function(callback){
 		if(err){
 			return callback(err);
 		}
-		db.collection('users',function(err,collection){
+		db.collection('chat',function(err,collection){
 			if(err){
 				mongodb.close();
 				return callback(err);
 			}
 
 			collection.insert(info,{safe:true},function(err,info){
+				mongodb.close();
+				callback(null);
+			});
+
+		});
+	});
+}
+
+Chat.get = function(key,callback){
+
+	mongodb.open(function(err,db){
+		if(err){
+			return callback(err);
+		}
+		db.collection('chat',function(err,collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+
+			collection.find(key,{safe:true},function(err,data){
 				mongodb.close();
 				callback(null);
 			});
