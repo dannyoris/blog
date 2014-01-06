7c254f9ce4260c78cf798a094e66c7a06fc4e313
dannyb <dannyb.liu@sh.leoburnett.com>
2014-01-06 12:53:24 +0800

    f

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var express = require('express'),
 
 var app = express(),server;
 
-// all environments
+// all environment
 app.set('port', process.env.PORT || 28);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
